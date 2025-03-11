d73d92fa21416798b49c819e9d79ca41c3881c18
karynnemoreira <karynnemoreira@yahoo.com.br>
2025-03-11 18:50:17 -0300

    passos para a correção do vercel

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
   server: {
-    historyApiFallback: true, 
+    historyApiFallback: true,  // Garante que o React Router gerencia as rotas
   },
 })
